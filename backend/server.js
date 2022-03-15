a1fcaaf734af7460af22332c99ed65e718462087
nina011 <javieraburgospaez@gmail.com>
2022-03-15 06:28:58 -0300

    se ha actualizado el archivo server

--- backend/server.js
+++ backend/server.js
@@ -18,29 +18,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/users', userRoutes)
 
-// if(process.env.NODE_ENV = 'production'){
 
-//     app.use(express.static(path.join(__dirname,'../frontend/build')))
-
-//     app.get('*', (req, res) => res.sendFile(__dirname,'../','frontend',
-//     'build','index.html'))
-// }else{
     app.get('/', (req, res) => {
         res.json({msg: 'Bienvenido a la API - Javiera Burgos'})
     })
-// }
+
 
 app.use(errorHandler)
 
-// app.use((err, req, res, next ) => {
-//     console.log('EN EL ERROR HANDLER SERVER', err)
-//     const statusCode = res.statusCode ? res.statusCode : 500
-//     res.status(statusCode)
-//     res.json({
-//         message: err.message,
-//         stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-// })
-// })
 
 app.listen(PORT, () => console.log(`Servidor en el puerto ${PORT}`))
 
